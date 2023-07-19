576e907da9e3bde60b6e334bad8126e2cbb4d590
Abhishek Kumar <abhishekverman3459@gmail.com>
2023-07-19 15:30:52 +0530

    Allowing Access to Backend

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://bookyourplace.onrender.com",
   })
 );
 app.use(cookieParser());
